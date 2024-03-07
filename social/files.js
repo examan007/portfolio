db41c4a90c42788076bb248897b91ba3eefd4580
James Hayes <exajames@gmail.com>
2024-03-07 16:41:15 +0000

    facebook feed plugin

--- social/files.js
+++ social/files.js
@@ -5,3 +5,37 @@ var FileList = function () { return [
   "split_25.png",
   "split_31.png"
 ]}
+var MetricList = function () { return [
+  {
+    "name": "split_7.png",
+    "start": 84,
+    "orig-height": 656,
+    "height": 635,
+  },
+  {
+    "name": "split_13.png",
+    "orig-start": 745,
+    "start": 790,
+    "orig-height": 487,
+    "height": 420,
+  },
+  {
+    "name": "split_25.png",
+    "orig-start": 1761,
+    "orig-height": 815,
+    "start": 2120,
+    "height": 820,
+  },
+  {
+    "name": "split_19.png",
+    "orig-start": 1237,
+    "orig-height": 519,
+    "start": 1280,
+    "height": 765,
+  },
+  {
+    "name": "split_31.png",
+    "start": 2581,
+    "height": 465,
+  }
+]}
