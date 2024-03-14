68c7be6b2baf314faf9a9399dddbee1e666cdb85
James Hayes <exajames@gmail.com>
2024-03-14 23:16:16 +0000

    update metrics

--- social/files.js
+++ social/files.js
@@ -10,37 +10,37 @@ var MetricList = function () { return [
     "name": "split_7.png",
     "orig": {
         "start": 84,
-        "height": 596
+        "height": 690
     },
     "start": 68,
-    "height": 482
+    "height": 595
   },
   {
     "name": "split_13.png",
     "orig": {
-        "start": 685,
-        "height": 656
+        "start": 779,
+        "height": 412
     },
-    "start": 612,
-    "height": 511
+    "start": 725,
+    "height": 305
   },
   {
     "name": "split_19.png",
     "orig": {
-        "start": 1346,
-        "height": 487
+        "start": 1196,
+        "height": 596
     },
-    "start": 1183,
-    "height": 336
+    "start": 1090,
+    "height": 482
   },
   {
     "name": "split_25.png",
-    "start": 1838,
-    "height": 519
+    "start": 1797,
+    "height": 656
   },
   {
     "name": "split_31.png",
-    "start": 2362,
-    "height": 815
+    "start": 2458,
+    "height": 487
   }
 ]}
